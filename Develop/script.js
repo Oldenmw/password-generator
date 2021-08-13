196131065b67cbb12e9ddd06ac5dbf4e0a84627c
Matt Oldenburg <theoldenmw@gmail.com>
2021-08-13 10:53:34 -0400

    added initial js

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,24 @@
 // Assignment code here
+var length = 8;
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var symbols = " !@#$%^&*()_-+=,.<>?~`/;:";
 
+var generatePassword = function() {
+  //get password length
+  length = window.prompt("How many characters would you like your password to be? Choose between 8 and 128 characters.");
+  length = parseInt(length);
+  console.log(length);
+
+  if (!length || length < 8 || length > 128) {
+    window.alert("Please enter a whole number between 8 and 128.")
+    generatePassword();
+  };
+
+  //ask if user wants uppercase/lowercase/numbers/symbols
+  
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
